358290373643277727ac66eeaa0dd19f68ce01fd
FranciscoAbad <unlafranciscoabad@gmail.com.ar>
2023-09-07 14:29:57 +0200

    Backend set

---
+++ backend/matriz-backend/src/main/java/com/plancton/MatrizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plancton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrizBackendApplication.class, args);
+	}
+
+}
---
+++ backend/matriz-backend/src/test/java/com/plancton/MatrizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plancton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
